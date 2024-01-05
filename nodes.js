902d5ced77d296694f145d7c3b5ae458b3009a23
StarlightJS <150332743+StarlightJS@users.noreply.github.com>
2024-01-05 07:40:46 +0100

    Update nodes.js

--- nodes.js
+++ nodes.js
@@ -6,6 +6,13 @@
       "port": 21001,
       "ssl": false,
       "cache": false
+    },
+    {
+      "name": "Second Official Nolanium Node",
+      "url": "nolanium.xyz",
+      "port": 21101,
+      "ssl": false,
+      "cache": false
     }
   ]
 }
